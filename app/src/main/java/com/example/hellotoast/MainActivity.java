b50c4f140b884ca9dc211ab77d15bcb5ad8e8b1a
CrisM93 <criztian.gio@gmail.com>
2022-12-15 17:59:18 -0600

    Zero terminada

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 //ESTOY DESDE ANDRIOD2.1.._Zero
 public class MainActivity extends AppCompatActivity {
 
-    private int mCount=10;
+    private int mCount=0;
     private TextView mShowCount;
 
 
@@ -26,29 +26,34 @@ public class MainActivity extends AppCompatActivity {
     mShowCount=(TextView) findViewById(R.id.show_count);
     }
 
-    @Override
+    /*@Override
     protected void onResume() {
         super.onResume();
         Log.d("Mainactivity", "onresume_checar en un log" );
-    }
+    }*/
 
     public void showToast(View view) {
      Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
        toast.show();
     }
 
+    public void zeroUp(View view){
+        Toast toast2 = Toast.makeText(this, R.string.toast_message2,Toast.LENGTH_SHORT);
+        toast2.show();
+        mCount=0;
+        if(mShowCount !=null) {
+            mShowCount.setText(Integer.toString(0));
+        }
+
+    }
+
     public void countUp(View view) {
 
-        mCount--;
-        if(mCount>-7){
+        mCount++;
         if(mShowCount !=null) {
             mShowCount.setText(Integer.toString(mCount));
         }
-        }//solo hasta -7 se queda el descontador en -6
-        else{
-            Toast toast2 = Toast.makeText(this, R.string.toast_message2,Toast.LENGTH_SHORT);
-            toast2.show();
-        }
+
 
     }
 }
